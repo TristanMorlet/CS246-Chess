d21c90e84753d7a7b9b10bb40c75fb2cb38ac9ec
tvrdsmor <tvrdsmor@uwaterloo.ca>
2025-07-19 14:03:41 -0400

    Board changes

--- Model/Board.h
+++ Model/Board.h
@@ -1,3 +1,6 @@
+#ifndef BOARD_H
+#define BOARD_H
+
 #include <algorithm>
 #include <utility>
 #include <iostream>
@@ -5,9 +8,43 @@
 #include <sstream>
 
 
-#include "observer.h"
+#include "../View/observer.h"
+#include "Piece.h"
 
 using namespace std;
 
+enum class Colour{
+    White,
+    Black,
+    None
+};
 
-class Board{  }
+struct Move{
+    string to;
+    string from;
+}
+
+class Board{  
+    vector<Observer *> observers;
+
+    public:
+
+
+
+    void setupPiece(char piece, string coord);
+    bool isMoveValid(Move move);
+    void applyMove(Move move);
+    void removePiece(string coord);
+    void setTurn(Colour colour);
+
+    void attach(Observer* o);
+    void detach(Observer* o);   
+    void notifyObservers();
+    
+    // getters and setters
+    Piece* getPieceAt(string coordinate) const {
+        if (Board[coordinate]) {}
+    }
+};
+
+#endif
