dcb752008892c8f283d60f90b39f925ab30dbf4c
a88wong <a88wong@uwaterloo.ca>
2025-07-19 14:50:23 -0400

    get char rep

--- Model/Piece.cc
+++ Model/Piece.cc
@@ -22,5 +22,15 @@ std::vector<Move> Queen::getValidMoves(const Board &b) const {
 }
 
 std::vector<Move> King::getValidMoves(const Board &b) const {
-    return {};
+    std::vector<Piece::Move> mv;
+    static const int moveRow[8] = {-1,-1,-1,0,0,1,1,1}; //Possible row/col offsets (moveRow[i] + )
+    static const int moveCol[8] = {-1,0,1,-1,1,-1,0,1}; 
+    for (int i = 0; i < 8; ++i) {
+        int newRow = row + moveRow[i];
+        int newCol = col + moveCol[i];
+        if (nr < 0 || nr >= 8 || nc < 0 || nc >= 8) continue;
+        Piece *p = b.getPieceAt(nr,nc);
+        if (!p || p->getColour() != colour) mv.push_back({row, col, nr, nc});
+    }
+    return mv;
 }
--- Model/Piece.h
+++ Model/Piece.h
@@ -7,59 +7,79 @@ class Board;
 
 class Piece { 
 protected:
-    Piece(Colour colour, int row, int col): colour{colour}, row{row}, col{col} {}
-    Colour colour;
+    Piece(Colour colour, int row, int col): colour{colour}, row{row}, col{col} {} //define piece
+    Colour colour; //define colour
     int row;
     int col;
 
 public:
     enum class Colour { White, Black };
-    struct Move { int fromRow, fromCol, toRow, toCol; };
+    struct Move { int fromRow, fromCol, toRow, toCol; }; //Move struct for moving pieces
 
-    virtual ~Piece() {}
+    virtual ~Piece() {} //dtor
 
     Colour getColour() const { return colour; }
     virtual std::vector<Move> getValidMoves(const Board&) const = 0;
+    virtual char getCharRepresentation() const = 0; //Get char representation for pieces
 };
 
 class Pawn : public Piece {
 public:
     Pawn(Piece::Colour c, int row, int col): Piece{c,row,col} {}
     std::vector<Move> getValidMoves(const Board &b) const override;
+    char getCharRepresentation() const override {
+        return colour == Colour::White ? 'P' : 'p'; //For representation, White pieces are capital, black pieces are lowercase
+    }
 };
 
 class Knight : public Piece {
     public:
         Knight(Piece::Colour c, int row, int col): Piece{c,row,col} {}
         std::vector<Move> getValidMoves(const Board &b) const override;
-    };
+        char getCharRepresentation() const override {
+            return colour == Colour::White ? 'N' : 'n';
+        }
+
+};
 
 
 class Bishop : public Piece {
     public:
         Bishop(Piece::Colour c, int row, int col): Piece{c,row,col} {}
         std::vector<Move> getValidMoves(const Board &b) const override;
-    };
+        char getCharRepresentation() const override {
+            return colour == Colour::White ? 'B' : 'b';
+        }
+};
 
 
 class Rook : public Piece {
     public:
         Rook(Piece::Colour c, int row, int col): Piece{c,row,col} {}
         std::vector<Move> getValidMoves(const Board &b) const override;
-    };
+        char getCharRepresentation() const override {
+            return colour == Colour::White ? 'R' : 'r';
+        }
+};
 
 
 class Queen : public Piece {
     public:
         Queen(Piece::Colour c, int row, int col): Piece{c,row,col} {}
         std::vector<Move> getValidMoves(const Board &b) const override;
+        char getCharRepresentation() const override {
+            return colour == Colour::White ? 'Q' : 'q';
+        }
     };
 
 class King : public Piece {
     public:
         King(Piece::Colour c, int row, int col): Piece{c,row,col} {}
         std::vector<Move> getValidMoves(const Board &b) const override;
-    };
+        char getCharRepresentation() const override {
+            return colour == Colour::White ? 'K' : 'K';
+        }
+};
 
 
 #endif
